412008ca59d3b8dacb1ab6262677857162eb09b7
kmoonwright <kmoonwright@outlook.com>
2019-12-12 16:10:43 -0800

    Add constructor for state

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default class App extends React.Component {
 
-  render () {
+  constructor() {
+    super()
+
+    this.state = {
+      todoInput: '',
+      todos: [
+        { id: 0, title: 'Take out the trash', done: false },
+        { id: 1, title: 'Cook dinner', done: false }
+      ]
+    }
+
+  }
+
+  render() {
     return (
       <View style={styles.container}>
-        <Text>This is a test!!!!</Text>
         <Text>This is a test!!!!</Text>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
+  
 }
 
 const styles = StyleSheet.create({
